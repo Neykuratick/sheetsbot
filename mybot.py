b0ca349ec253033f9da1a9b028ab2f2aa81dd914
Neykuratick <neykuratick@yahoo.com>
2020-11-01 10:34:44 +0300

    make it better

--- mybot.py
+++ mybot.py
@@ -232,16 +232,16 @@ def keyboardNextWeek():
     return markup
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return '!', 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://mybot69420.herokuapp.com/' + TOKEN)
-    return '!', 200
+# @server.route('/' + TOKEN, methods=['POST'])
+# def getMessage():
+#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#     return '!', 200
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url='https://mybot69420.herokuapp.com/' + TOKEN)
+#     return '!', 200
 
 
 while True:
@@ -249,6 +249,6 @@ while True:
         bot.polling()
     except:
         pass
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+#
+# if __name__ == "__main__":
+#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
